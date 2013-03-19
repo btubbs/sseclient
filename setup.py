59dac638d5ab4583d301e0d5553cc475d3fa55f4
Brent Tubbs <brent.tubbs@gmail.com>
2013-03-19 13:15:07 -0700

    bump requests requirement

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='sseclient',
-    version='0.0.3',
+    version='0.0.4',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     py_modules=['sseclient'],
-    install_requires=['requests==0.14.2'],
+    install_requires=['requests>=1.0.4,<2.0'],
     description=(
         'Python client library for reading Server Sent Event streams.'),
     long_description=open('README.rst').read(),
