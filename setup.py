15b48e34609b04efedbd7d95352962ebd646bfbc
Brent Tubbs <brent.tubbs@gmail.com>
2013-10-21 17:17:24 -0700

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sseclient',
-    version='0.0.5',
+    version='0.0.6',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     py_modules=['sseclient'],
