9a8852dffe75c07c1fbd0cd7d9662a717d501fb4
Brent Tubbs <brent.tubbs@gmail.com>
2013-10-21 17:16:23 -0700

    loosen up requests versioning

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='sseclient',
-    version='0.0.4',
+    version='0.0.5',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     py_modules=['sseclient'],
-    install_requires=['requests>=1.0.4,<2.0'],
+    install_requires=['requests>=1.0.4'],
     description=(
         'Python client library for reading Server Sent Event streams.'),
     long_description=open('README.rst').read(),
