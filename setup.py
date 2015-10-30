6e86334f348f71182c36920b78cd3042ce4115cd
Brent Tubbs <brent.tubbs@gmail.com>
2015-10-30 10:00:39 -0600

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ pytest_runner = ['pytest_runner>=2.1'] if needs_pytest else []
 
 setup(
     name='sseclient',
-    version='0.0.10',
+    version='0.0.11',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     py_modules=['sseclient'],
